d7bd5111d67e334676f3be67914c0f24797d4e4a
Kristiina.konno <kristiina@GT-EST1010057.lan>
2020-01-26 14:06:06 +0200

    lib files updated and confidentaility note added to test file

--- lib/common/publication/PublicationsFileFactory.d.ts
+++ lib/common/publication/PublicationsFileFactory.d.ts
@@ -3,7 +3,7 @@ import { PublicationsFile } from './PublicationsFile';
  * Publications file factory for publications file creation from byte array
  */
 export declare class PublicationsFileFactory {
-    private trustedCertificates;
+    private readonly trustedCertificates;
     constructor(trustedCertificates?: string);
     create(publicationFileBytes: Uint8Array): PublicationsFile;
 }
